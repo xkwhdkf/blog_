185b1803d826bd018ed011d64fbd751206c0b2a7
Kim jin hyang <73262330+xkwhdkf@users.noreply.github.com>
2023-08-28 00:58:10 +0900

    08/28

--- src/components/RenderHeader.tsx
+++ src/components/RenderHeader.tsx
@@ -1,20 +1,22 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { addMonths, subMonths } from 'date-fns';
-import { format } from 'path'
+import { addDays, addMonths, endOfMonth, endOfWeek, isSameDay, isSameMonth, startOfMonth, startOfWeek, subMonths } from 'date-fns';
+import { format, parse } from 'path'
 import React, { useState } from 'react'
 
 
 
-const RenderHeader = ({ currentMonth }:{currentMonth: any}) => {
+
+
+const RenderHeader = ({ currentMonth }:{currentMonth: any}, {prevMonth}: {prevMonth: any}, {nextMonth}: {nextMonth: any}) => {
   return (
     <div className='headerRow'>
       <div className='col-start'>
         <span className='text'>
           <span className='textMonth'>
-            {format(currentMonth)}월
+            {format(currentMonth, 'M')}월
           </span>
-          {format(currentMonth)}
+          {format(currentMonth, 'yyyy')}
         </span>
       </div>
       <div className='col-end'>
@@ -25,15 +27,81 @@ const RenderHeader = ({ currentMonth }:{currentMonth: any}) => {
   );
 };
 
+const RenderDay= () => {
+  const day = [];
+  const date = ['일', '월', '화', '수', '목', '금' ];
+
+  for(let i=0; i<7; i++) {
+    day.push(
+      <div className='col' key={i}>
+        {date[i]}
+      </div>
+    );
+  }
+  return <div className='day row'>{day}</div>
+}
+
+const RenderCells = ( {currentMonth}: {currentMonth: any}, {selectedDate}: {selectedDate: any}, {onDateClick}: {onDateClick: any} ) => {
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = '';
+
+  while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+          formattedDate = format(day, 'd');
+          const cloneDay = day;
+          days.push(
+              <div
+                  className={`col cell ${
+                      !isSameMonth(day, monthStart)
+                          ? 'disabled'
+                          : isSameDay(day, selectedDate)
+                          ? 'selected'
+                          : format(currentMonth, 'M') !== format(day, 'M')
+                          ? 'not-valid'
+                          : 'valid'
+                  }`}
+                  key={day}
+                  onClick={() => onDateClick(parse(cloneDay))}
+              >
+                  <span
+                      className={
+                          format(currentMonth, 'M') !== format(day, 'M')
+                              ? 'text not-valid'
+                              : ''
+                      }
+                  >
+                      {formattedDate}
+                  </span>
+              </div>,
+          );
+          day = addDays(day, 1);
+      }
+      rows.push(
+          <div className="row" key={day}>
+              {days}
+          </div>,
+      );
+      days = [];
+  }
+  return <div className="body">{rows}</div>;
+};
+
 export const Calender = () => {
-  const [currentMonth, setCurrentMont] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
   const [selectDate, setSelectDate] = useState(new Date());
 
-  const prevMonth  = () => {
-    setCurrentMont(subMonths(currentMonth, 1));
+  const prevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
   };
   const nextMonth = () => {
-    setCurrentMont(addMonths(currentMonth, 1));
+    setCurrentMonth(addMonths(currentMonth, 1));
   };
   const onDateClick = (day: any) => {
     setSelectDate(day);
@@ -44,6 +112,12 @@ export const Calender = () => {
         currentMonth={currentMonth}
         prevMonth={prevMonth}
         nextMonth={nextMonth}
+      />
+      <RenderDay />
+      <RenderCells
+        currentMonth={currentMonth}
+        selectedDate={selectDate}
+        onDateClick={onDateClick}
       />
     </div>
   )
